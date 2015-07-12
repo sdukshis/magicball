c82a85126949e26ec397c4aa7a4c03ff02eaef1e
Pavel Filonov <filonovpv@gmail.com>
2015-07-12 18:11:08 +0300

    load answers from JSON file

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,24 @@
 extern crate hyper;
 extern crate rand;
+extern crate rustc_serialize;
+#[macro_use]
+extern crate log;
+extern crate env_logger;
+
+use rustc_serialize::json;
 
 use std::io::Write;
+use std::io::Read;
 
 use hyper::Server;
 use hyper::server::Request;
 use hyper::server::Response;
 use hyper::net::Fresh;
 use rand::random;
+use std::fs::File;
 
 /// Magic ball answer structure
+#[derive(RustcDecodable, RustcEncodable, Debug)]
 struct Answer {
     /// Answer text
     text: String,
@@ -18,9 +27,17 @@ struct Answer {
 }
 
 /// Load posible answer and return then as array
-fn get_answers() -> Vec<Answer> {
-    vec![Answer{text: "Yes".to_string(), probability: 0.5},
-         Answer{text: "No".to_string(), probability: 0.5}]
+fn get_answers() -> Result<Vec<Answer>, &'static str> {
+    let mut data = String::new();
+    let mut f = File::open("answers.json");
+    if let Err(e) = f { 
+        return Err("Error open file");
+    }
+    if let Err(e) = f.unwrap().read_to_string(&mut data) {
+        return Err("Error read from file");
+    }
+    let answers: Vec<Answer> = json::decode(data.as_ref()).unwrap();
+    Ok(answers)
 }
 
 fn send_answer(_: Request, res: Response<Fresh>, answer: &[u8]) {
@@ -30,11 +47,18 @@ fn send_answer(_: Request, res: Response<Fresh>, answer: &[u8]) {
 }
 
 fn main() {
-    let answers = get_answers();
+    env_logger::init().unwrap();
+    info!("Loading answers");
+    let answers = get_answers().unwrap();
+    info!("Loaded {} answers", answers.len());
+    for answer in answers.iter() {
+        debug!("Answer: {:?}", answer);
+    }
     let handler = move |req: Request, res: Response<Fresh>| {
         let answer = answers.get(random::<usize>() % answers.len()).unwrap();
         send_answer(req, res, answer.text.as_bytes())
     };
 
+    info!("Starting server on 127.0.0.1:3000");
     Server::http("127.0.0.1:3000").unwrap().handle(handler);
 }
